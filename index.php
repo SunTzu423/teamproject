969fba29efe583c5a2b87457763283f0f53469a0
ElliotHiester <elliotawesome05@gmail.com>
2023-04-10 08:40:14 -0400

    t

--- index.php
+++ index.php
@@ -64,13 +64,13 @@
                     </div>
                 </div> 
                 <p class="main-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    <br><br>
+                    <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
+                    <!-- <br><br> -->
                     This website allows you to: 
-                    <ul class="main-text">
-                        <li>Check out and return books</li>
+                    <ul class="main-text">                        
                         <li>Browse from a collection of books</li>
                         <li>Keep track of your favorite books</li>
+                        <li>Add books to your personal collection by ISBN</li>
                         <li>And more</li>
                     </ul>
                 </p>
